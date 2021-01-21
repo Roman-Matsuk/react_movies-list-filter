88867daf111c7272ba67f282ad57ee9429d1fb3e
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-01-21 16:35:32 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.64e4bf7d2d31a10e3cc3224f1dfe75ae.js"
 );
 
 self.addEventListener('message', (event) => {
